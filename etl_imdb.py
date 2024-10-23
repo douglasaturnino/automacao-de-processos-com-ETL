d5116c07e4b138ae2232702d3b62bf01e528a9bb
Douglas Saturnino <douglasaturnino@gmail.com>
2024-10-23 16:33:30 -0300

    modulariza o projeto

--- etl_imdb.py
+++ etl_imdb.py
@@ -1,157 +1,160 @@
 # IMPORTS
-
-import os
-import requests
-import pandas as pd
-import sqlite3
 import logging
-import schedule
-import time
+import os
+import shutil
+import sqlite3
 
-# configuração do logging
-logging.basicConfig(
-    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+import pandas as pd
+import requests
+
+# Configuração do logging
+log_format = "%(asctime)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=log_format)
+
+# Adicione um manipulador de arquivo ao logger
+log_file_path = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "script_logs.log"
 )
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(logging.Formatter(log_format))
+logging.getLogger().addHandler(file_handler)
+
+BANCO_DADOS = "imdb_data.db"
+DESTINO_DIRETORIO = "data"
+BASE_URL = "https://datasets.imdbws.com/"
+ARQUIVOS = [
+    "name.basics.tsv.gz",
+    "title.akas.tsv.gz",
+    "title.basics.tsv.gz",
+    "title.crew.tsv.gz",
+    "title.episode.tsv.gz",
+    "title.principals.tsv.gz",
+    "title.ratings.tsv.gz",
+]
 
 
-def execute_script():
-    # EXTRAÇÃO DOS DADOS
-
-    # URL base para os datasets do IMDb
-    base_url = "https://datasets.imdbws.com/"
-
-    # Listas de nomes de arquivos que você deseja baixar
-    arquivos = [
-        "name.basics.tsv.gz",
-        "title.akas.tsv.gz",
-        "title.basics.tsv.gz",
-        "title.crew.tsv.gz",
-        "title.principals.tsv.gz",
-        "title.episode.tsv.gz",
-        "title.ratings.tsv.gz",
-    ]
-
-    # Diretório de destino
-    destino_diretorio = "data"
-
-    # Certifique-se de que o diretorio de destino existe
+def extract(
+    base_url: str = BASE_URL,
+    arquivos: list = ARQUIVOS,
+    destino_diretorio: str = DESTINO_DIRETORIO,
+) -> None:
+    logging.info("Início do processo de ETL")
     os.makedirs(destino_diretorio, exist_ok=True)
 
-    # Loop para baixar cada arquivo
     for arquivo in arquivos:
         url = base_url + arquivo
         caminho_destino = os.path.join(destino_diretorio, arquivo)
 
-        # Verifica se o arquivo já existe para evitar o download repetido
         if not os.path.exists(caminho_destino):
-            logging.info(f"Baixando {arquivo}.")
-            response = requests.get(url)
+            logging.info(f"Baixando {arquivo}...")
+            response = requests.get(url, stream=True)
 
-            # Verifica se a solicitação foi bem-sucedida (código de status 200)
             if response.status_code == 200:
                 with open(caminho_destino, "wb") as f:
-                    f.write(response.content)
-                logging.info(f"{arquivo} baixado com sucesso")
+                    shutil.copyfileobj(response.raw, f)
+                logging.info(f"{arquivo} baixado com sucesso!")
             else:
                 logging.error(
                     f"Falha ao baixar {arquivo}. Código de status: {response.status_code}"
                 )
+
+            del response
         else:
-            logging.info(f"{arquivo} já existe. Pulando o download")
+            logging.info(f"{arquivo} já existe. Pulando o download.")
+
     logging.info("Download concluído.")
 
-    # TRANSFORMAÇÃO DOS DADOS
 
-    # Tratamento dos dados
-
-    # Diretórios
+def transform(
+    arquivos: list = ARQUIVOS,
+    chunksize: int = 1_000_000,
+) -> None:
     diretorio_dados = "data"
     diretorio_tratados = os.path.join(diretorio_dados, "tratados")
 
-    # Certifica-se de que o diretório "tratados" existe
     os.makedirs(diretorio_tratados, exist_ok=True)
 
-    # Lista todos os arquivos no diretório "data"
-    arquivos = os.listdir(diretorio_dados)
-
-    # Loop para abrir, tratar e salvar cada arquivo
     for arquivo in arquivos:
         caminho_arquivo = os.path.join(diretorio_dados, arquivo)
 
         if os.path.isfile(caminho_arquivo) and arquivo.endswith(".gz"):
-            logging.debug(f"Lendo e tratando o arquivo {arquivo}.")
+            logging.debug(f"Lendo e tratando o arquivo {arquivo}...")
 
-            # Lê o arquivo TSV usando o pandas
-            df = pd.read_csv(
-                caminho_arquivo, sep="\t", compression="gzip", low_memory=False
+            chunks = pd.read_csv(
+                caminho_arquivo,
+                sep="\t",
+                compression="gzip",
+                low_memory=False,
+                chunksize=chunksize,
             )
 
-            # Substitui os caracteres '\n' por um valor nulo
-            df.replace({"\\N": None}, inplace=True)
+            for chunk in chunks:
+                chunk.replace({"\\N": None}, inplace=True)
 
-            # Salva o DataFrame no diretorio "tratados" sem compressão"
-            caminho_destino = os.path.join(
-                diretorio_tratados, arquivo[:-3]
-            )  # remove a extenão .gz
-            df.to_csv(caminho_destino, sep="\t", index=False)
+                caminho_destino = os.path.join(diretorio_tratados, arquivo[:-3])
+                chunk.to_csv(
+                    caminho_destino,
+                    sep="\t",
+                    mode="a",
+                    index=False,
+                    header=not os.path.exists(caminho_destino),
+                )
 
             logging.debug(
                 f"Tratamento concluído para {arquivo}. Arquivo tratado salvo em {caminho_destino}"
             )
 
+            # Remova o arquivo baixado após o tratamento
+            os.remove(caminho_arquivo)
+
     logging.info("Todos os arquivos foram tratados e salvos no diretório 'tratados'.")
 
-    # CARGA DE DADOS
 
-    # Diretório
+def load(conexao) -> None:
+    # CARGA DOS DADOS
     diretorio_tratados = os.path.join("data", "tratados")
-    banco_dados = "imdb_data.db"
 
-    # Conecta ao banco de dados SQLite
-    conexao = sqlite3.connect(banco_dados)
-
-    # Lista todos os arquivos no diretório "tratados"
     arquivos = os.listdir(diretorio_tratados)
 
-    # Loop para ler cada arquivo e salvar em uma tabela SQLite
     for arquivo in arquivos:
         caminho_arquivo = os.path.join(diretorio_tratados, arquivo)
 
         if os.path.isfile(caminho_arquivo) and arquivo.endswith(".tsv"):
-            # Lê o arquivo TSV usando o pandas
             df = pd.read_csv(caminho_arquivo, sep="\t", low_memory=False)
 
-            # Remove a extensão do nome do arquivo
             nome_tabela = os.path.splitext(arquivo)[0]
-
-            # Substitui os caracteres especiais no nome da tebela
             nome_tabela = nome_tabela.replace(".", "_").replace("-", "_")
 
-            # Salva o DataFrame na tabela SQLite
             df.to_sql(nome_tabela, conexao, index=False, if_exists="replace")
 
             logging.info(
                 f"Arquivo {arquivo} salvo como tabela {nome_tabela} no banco de dados."
             )
-    # Fecha a conexao com o banco de dados
-    conexao.close()
 
-    logging.info("Todos os arquivos foram salvos no banco de dados")
+            # Remova o arquivo tratado após a carga no banco de dados
+            os.remove(caminho_arquivo)
 
-    # CRIAÇÃO DAS TABELAS ANALITICAS
+    logging.info("Todos os arquivos foram salvos no banco de dados.")
+
+
+def create_views(conexao) -> None:
+    # CRIAÇÃO DAS TABELAS ANALÍTICAS
     analitico_titulos = """
-    CREATE TABLE IF NOT EXISTS analitico_titulos AS 
+    CREATE TABLE IF NOT EXISTS analitico_titulos AS
 
     WITH 
     participantes AS (
-        SELECT 
+        SELECT
             tconst,
-            COUNT(DISTINCT nconst) AS qtParticipantes
-        FROM title_principals 
-        GROUP BY tconst
+            COUNT(DISTINCT nconst) as qtParticipantes
+        
+        FROM title_principals
+        
+        GROUP BY 1
     )
 
-    SELECT 
+    SELECT
         tb.tconst,
         tb.titleType,
         tb.originalTitle,
@@ -161,57 +164,57 @@ def execute_script():
         tr.averageRating,
         tr.numVotes,
         tp.qtParticipantes
+
     FROM title_basics tb 
 
     LEFT JOIN title_ratings tr
-        ON (tr.tconst = tb.tconst)
+        ON tr.tconst = tb.tconst
 
     LEFT JOIN participantes tp
-        ON (tp.tconst = tb.tconst)
-
+        ON tp.tconst = tb.tconst
     """
 
     analitico_participantes = """
-    CREATE TABLE IF NOT EXISTS analitico_participantes AS 
+    CREATE TABLE IF NOT EXISTS analitico_participantes AS
 
-    SELECT 
-    tp.nconst,
-    tp.tconst,
-    tp.ordering,
-    tp.category,
-    tb.genres
+    SELECT
+        tp.nconst,
+        tp.tconst,
+        tp.ordering,
+        tp.category,
+        tb.genres
+
     FROM title_principals tp
 
     LEFT JOIN title_basics tb
-    ON (tb.tconst = tp.tconst)
+        ON tb.tconst = tp.tconst
     """
 
-    # lista de consultas
-    queries = [analitico_titulos, analitico_participantes]
+    queries = [
+        "DROP TABLE IF EXISTS analitico_titulos",
+        "DROP TABLE IF EXISTS analitico_participantes",
+        analitico_titulos,
+        analitico_participantes,
+    ]
 
-    # Salva as tabelas analiticas no banco de dados
+    logging.info("Salvando tabelas anlíticas no banco de dados.")
+
     for query in queries:
-        # Diretórios
-        banco_dados = "imdb_data.db"
-
-        # Conecta ao banco de dados SQLite
-        conexao = sqlite3.connect(banco_dados)
-
-        # Consulta SQL para contar o número de pessoas participantes por título
-        query = query
-
-        # Executa a consulta SQL
         conexao.execute(query)
 
-        # Fecha a conexao com o banco de dados
-        conexao.close()
+    logging.info("Tabelas analíticas criadas com sucesso.")
 
-    logging.info("Tabelas criadas com sucesso.")
+    logging.info("Fim do processo de ETL")
 
 
-# Agenda a execução do script
-schedule.every().day.at("09:00").do(execute_script)
+if __name__ == "__main__":
+    extract()
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    transform()
+
+    conexao = sqlite3.connect(BANCO_DADOS)
+
+    load(conexao=conexao)
+    create_views(conexao=conexao)
+
+    conexao.close()

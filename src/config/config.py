dc1290ad5c4f97d585c9c364df01474086ab90b7
Douglas Saturnino <douglasaturnino@gmail.com>
2024-10-24 04:55:43 -0300

    Cria a classe config

---
+++ src/config/config.py
@@ -0,0 +1,13 @@
+class config:
+    BANCO_DADOS = "imdb_data.db"
+    DIRETORIO_DADOS = "data"
+    BASE_URL = "https://datasets.imdbws.com/"
+    ARQUIVOS = [
+        "name.basics.tsv.gz",
+        "title.akas.tsv.gz",
+        "title.basics.tsv.gz",
+        "title.crew.tsv.gz",
+        "title.episode.tsv.gz",
+        "title.principals.tsv.gz",
+        "title.ratings.tsv.gz",
+    ]
